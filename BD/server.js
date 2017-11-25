72801e96d690bc7c8decdac9357c94d2d5581290
Licinius <florent.dellomo@gmail.com>
2017-11-25 14:13:18 +0100

    NodeJs , le server
    Definition de la fonction trajetResearch permettant de chercher des trajets avec les bons paramètres

--- BD/server.js
+++ BD/server.js
@@ -1,18 +1,28 @@
-var url = "mongodb://localhost:27017/cov-urage";
-var http = require("http");
+let express = require ("express");
+let cors = require ("cors");
+let app = expres();
 var assert = require('assert');
 var MongoClient = require("mongodb").MongoClient;
+
+
+app.listen(8888);
+app.use(cors());
+
+let mongoClient = require("mongodb").MongoClient;
+let ObjectId = require("mongodb").ObjectId;
+
+let url = "mongodb://localhost:27017/cov-urage";
+
+function trajetResearch(db,param,callback){
+	db.collection("trajets").find(["filterObject"]).toArray(function(err,documents){
+		if (documents !== undefined) 
+			callback(param["message"],documents);
+		else
+			callback(param["message"],[]);
+	});
+}
 
 MongoClient.connect(url, function(error, db) {
     assert.equal(null,error);
     console.log("Connecté à la base de données cov-urage");
 });
-
-var server = http.createServer(function(req, res) {
-
-  res.writeHead(200);
-
-  res.end('Salut tout le monde !');
-
-});
-server.listen(8055);
