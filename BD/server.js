68e8d164be19c8c6c7c74aa33b8a503c256cb0bf
fdellomo <fdellomo@s32.fdsetu.infra.umontpellier.fr>
2017-12-01 10:37:29 +0100

    Avancé server node js
    On peut chercher les trajets correspondants à un prix particulier

--- BD/server.js
+++ BD/server.js
@@ -1,8 +1,7 @@
 let express = require ("express");
 let cors = require ("cors");
 let app = express();
-var assert = require('assert');
-var MongoClient = require("mongodb").MongoClient;
+let assert = require('assert');
 
 
 app.listen(8888);
@@ -14,26 +13,60 @@ let ObjectId = require("mongodb").ObjectId;
 let url = "mongodb://localhost:27017/cov-urage";
 
 function trajetResearch(db,param,callback){
-	db.collection("trajets").find(["filterObject"]).toArray(function(err,documents){
-		if (documents !== undefined) 
+    console.log("trajetResearch");
+    console.log(param["filterObject"]);
+	db.collection("trajets").find(param["filterObject"]).toArray(function(err,documents){
+        if (err)
+            callback(err,[]);
+		else if (documents !== undefined) 
 			callback(param["message"],documents);
 		else
 			callback(param["message"],[]);
 	});
 }
 
-MongoClient.connect(url, function(error, db) {
+
+function trajetPrix(db,param,callback){
+    db.collection("trajets").find(param["filterObject"]).toArray(function(err,documents){
+        if (err)
+            callback(err,[]);
+        else if (documents !== undefined) 
+            callback(param["message"],documents);
+        else
+            callback(param["message"],[]);
+    });
+}
+mongoClient.connect(url, function(error, db) {
     assert.equal(null,error);
     console.log("Connecté à la base de données cov-urage");
-    app.get("/trajets/villeD/villeA",function(req,res){
+    app.get("/trajets/:villeD/:villeA",function(req,res){
     	let filterObject = {};
+        filterObject.depart ={};
+        filterObject.arrive={};
     	if(req.params.villeD != "*"){filterObject.depart.ville = req.params.villeD;}
     	if(req.params.villeA != "*"){filterObject.arrive.ville = req.params.villeA;}
+        console.log(filterObject.depart.ville + "->" + filterObject.arrive.ville );
+
+
     	trajetResearch(db,{"message" : "/trajets","filterObject": filterObject},function(step,results){
-    		console.log(step + "avec" + results.length + "trajets selectionnés : ");
+    		console.log("\n" + step + "avec" + results.length + "trajets selectionnés : ");
     		res.setHeader("Content-type","application/json; charset = UTF-8");
     		let json = JSON.stringify(results);
+            console.log(json);
     		res.end(json);
-    	})
+    	});
+    });
+
+    app.get("/trajets/:prix",function(req,res){
+        let filterObject = {};
+        if(req.params.prix != "*"){filterObject.prix = parseInt(req.params.prix);}
+
+        trajetPrix(db,{"message" : "/trajets","filterObject": filterObject},function(step,results){
+            console.log("\n" + step + "avec" + results.length + "trajets selectionnés : ");
+            res.setHeader("Content-type","application/json; charset = UTF-8");
+            let json = JSON.stringify(results);
+            console.log(json);
+            res.end(json);
+        });
     });
 });
