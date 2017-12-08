e704fde1f41acc33969133beb8e41cdd95f14892
ToWaR6 <loic.crauser@free.fr>
2017-12-08 11:16:52 +0100

    j'add app
    t'as compirs c'est le dossier app

--- cov-urage/src/app/app.module.ts
+++ cov-urage/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {TrajetService} from './trajet.service';
+import {TrajetService} from './journey/trajet.service';
 import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { DisplayJourneyComponent } from './display-journey/display-journey.component';
+import { DisplayJourneyComponent } from './journey/display-journey/display-journey.component';
 
 @NgModule({
   declarations: [
